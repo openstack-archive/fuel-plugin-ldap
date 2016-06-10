20411ed8bbc3566dcc0b37a2ad758c9102a5197d
Maksym Yatsenko <myatsenko@mirantis.com>
2016-06-11 00:51:13 +0300

    LDAP proxy support
    it enables support of ldap proxy in HA-mode.
    
    Change-Id: I24bb5c8033d202dfc56d3779c93b42a36881d95b

---
+++ deployment_scripts/puppet/modules/plugin_ldap/manifests/tls.pp
@@ -0,0 +1,27 @@
+define plugin_ldap::tls (
+  $domain_tls = undef,
+  $ca_chain   = undef,
+){
+
+    $cacertfile = "/usr/local/share/ca-certificates/cacert-ldap-${domain_tls}.crt"
+
+    if $ca_chain {
+      $tls_cacertdir = '/etc/ssl/certs'
+    }
+    else {
+      $tls_cacertdir = ''
+    }
+
+    if $ca_chain {
+      file { $cacertfile:
+        ensure  => file,
+        mode    => 0644,
+        content => $ca_chain,
+      }
+      ~>
+      exec { "$domain_tls" :
+        command => '/usr/sbin/update-ca-certificates'
+      }
+    }
+
+}
