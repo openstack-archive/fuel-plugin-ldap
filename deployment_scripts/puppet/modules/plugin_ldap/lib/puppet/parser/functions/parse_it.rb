680cdd699c62ce56f31118a4861d9e608ecd15e3
Mykyta Karpin <mkarpin@mirantis.com>
2017-01-26 19:27:27 +0200

    Rewrite additional domains generation
    This patch makes use of Puppet native function
    create_resources() in order to generate
    Keystone domain resources from hash
    provided by parce_it() function.
    
    This approach required modification of parce_it()
    function so it can parse list of additional domains strings
    and generate a hash in form of:
    
    domain1_name => { property1 => value1,
                      property2 => value2,
                      .....
                      propertyx => valuex },
    domain2_name => { property1 => value1,
                      property2 => value2,
                      .....
                      propertyx => valuex },
    .....and so on
    
    This form of hash is suitable to be taken by create_resources()
    function. Puppet define plugin_ldap::multiple_domain
    was also modified to comply with create_resources()
    function.
    
    Change-Id: I14321af5efa18f1381a51668ed1c5c50c06a0002
    Closes-Bug: #1658655

--- deployment_scripts/puppet/modules/plugin_ldap/lib/puppet/parser/functions/parse_it.rb
+++ deployment_scripts/puppet/modules/plugin_ldap/lib/puppet/parser/functions/parse_it.rb
@@ -1,25 +1,32 @@
 module Puppet::Parser::Functions
   newfunction(:parse_it, :type => :rvalue, :doc => <<-EOS
-This function parses text area, create hash and returns values
-for keystone domain creation
+This function parses text area, creates hash and returns it
+for keystone domains creation
 EOS
   ) do |args|
 
-    param_hash = {}
-    cert_chain = args[0].slice!(/^(ca_chain=-----BEGIN CERTIFICATE-----)(.*[\r\n])+(-----END CERTIFICATE-----[\s\S]*?)$/)
+    domains_hash = {}
 
-    if cert_chain
-      splited_cert_chain = cert_chain.split('=',2)
-      param_hash[splited_cert_chain[0]] = splited_cert_chain[1]
+    args[0].each do |item|
+      param_hash={}
+      cert_chain = item.slice!(/^(ca_chain=-----BEGIN CERTIFICATE-----)(.*[\r\n])+(-----END CERTIFICATE-----[\s\S]*?)$/)
+
+      if cert_chain
+        splited_cert_chain = cert_chain.split('=',2)
+        param_hash[splited_cert_chain[0]] = splited_cert_chain[1]
+      end
+
+      splited_text = item.split("\n")
+      splited_text.each do |param|
+        splited_line = param.split('=',2)
+        if splited_line[0] and splited_line[0] != :undef
+          param_hash[splited_line[0]] = splited_line[1]
+        end
+      end
+      domains_hash[param_hash['domain']] = param_hash
     end
 
-    splited_text = args[0].split("\n")
-    splited_text.each do |item|
-      splited_line = item.split('=',2)
-      param_hash[splited_line[0]] = splited_line[1]
-    end
-
-    return param_hash
+    return domains_hash
   end
 end
 
